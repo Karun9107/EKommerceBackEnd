41e7eef7d4150dccced58be3cbb05913786c7393
Karun <karunrajagopal9107@gmail.com>
2021-12-19 19:59:53 +0530

    Exposed default REST endpoints using spring-data-rest

---
+++ src/main/java/com/ekommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.ekommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.ekommerce.model.ProductCategory;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path="product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+}
--- src/main/java/com/ekommerce/model/ProductCategory.java
+++ src/main/java/com/ekommerce/model/ProductCategory.java
@@ -24,7 +24,7 @@ public class ProductCategory {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private Long id;
-	@Column(name="cotegory_name")
+	@Column(name="category_name")
 	private String categoryName;
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
 	private Set<Product> products;
