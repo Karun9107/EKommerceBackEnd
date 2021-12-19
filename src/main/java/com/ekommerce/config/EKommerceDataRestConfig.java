107ff3707c02563947d44ada8b51e018718d5311
Karun <karunrajagopal9107@gmail.com>
2021-12-19 20:32:27 +0530

    Added configuration to make APIs read-only

---
+++ src/main/java/com/ekommerce/config/EKommerceDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.ekommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.ekommerce.model.Product;
+import com.ekommerce.model.ProductCategory;
+
+@Configuration
+public class EKommerceDataRestConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		HttpMethod theUnsupportedActions[] = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
+		config.getExposureConfiguration()
+		.forDomainType(Product.class)
+		.withItemExposure((metadata, httpMethods)->httpMethods.disable(theUnsupportedActions))
+		.withCollectionExposure((metadata, httpMethods)->httpMethods.disable(theUnsupportedActions));
+		
+		config.getExposureConfiguration()
+		.forDomainType(ProductCategory.class)
+		.withItemExposure((metadata, httpMethods)->httpMethods.disable(theUnsupportedActions))
+		.withCollectionExposure((metadata, httpMethods)->httpMethods.disable(theUnsupportedActions));
+	}
+
+}
