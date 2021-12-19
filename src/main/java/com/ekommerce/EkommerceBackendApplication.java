17706a4ba861991be2ae5e698ae8f39502d5e2f9
Karun <karunrajagopal9107@gmail.com>
2021-12-19 14:08:53 +0530

    Setup project structure

---
+++ src/main/java/com/ekommerce/EkommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ekommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkommerceBackendApplication.class, args);
+	}
+
+}
