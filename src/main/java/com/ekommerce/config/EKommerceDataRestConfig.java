b25bcf006afd55d4ea29dafc104661bf68861aa6
Karun <karunrajagopal9107@gmail.com>
2021-12-27 00:18:56 +0530

    Exposed id's of entities in response

--- src/main/java/com/ekommerce/config/EKommerceDataRestConfig.java
+++ src/main/java/com/ekommerce/config/EKommerceDataRestConfig.java
@@ -1,5 +1,13 @@
 package com.ekommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -11,7 +19,10 @@ import com.ekommerce.model.ProductCategory;
 
 @Configuration
 public class EKommerceDataRestConfig implements RepositoryRestConfigurer {
-
+	
+	@Autowired
+	EntityManager entityManager;
+	
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		HttpMethod theUnsupportedActions[] = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
@@ -24,6 +35,20 @@ public class EKommerceDataRestConfig implements RepositoryRestConfigurer {
 		.forDomainType(ProductCategory.class)
 		.withItemExposure((metadata, httpMethods)->httpMethods.disable(theUnsupportedActions))
 		.withCollectionExposure((metadata, httpMethods)->httpMethods.disable(theUnsupportedActions));
+		exposeIds(config);
+	}
+	
+	private void exposeIds(RepositoryRestConfiguration config) {
+		
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+		
+		List<Class<?>> entityList  = new ArrayList<>();
+		
+		for(EntityType<?> entity : entities) {
+			entityList.add(entity.getJavaType());
+		}
+		
+		config.exposeIdsFor(entityList.toArray( new Class[0]));
 	}
 
 }
