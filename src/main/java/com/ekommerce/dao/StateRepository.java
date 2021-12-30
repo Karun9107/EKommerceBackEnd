de5c45f74f8594343cc887f41d27d63be2d0f355
Karun <karunrajagopal9107@gmail.com>
2021-12-30 12:46:49 +0530

    Added api to list states by country code

--- src/main/java/com/ekommerce/dao/StateRepository.java
+++ src/main/java/com/ekommerce/dao/StateRepository.java
@@ -1,9 +1,13 @@
 package com.ekommerce.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ekommerce.model.State;
 
@@ -11,5 +15,5 @@ import com.ekommerce.model.State;
 @Repository
 @RepositoryRestResource(collectionResourceRel = "states", path="state")
 public interface StateRepository extends JpaRepository<State, Integer> {
-
+	List<State> findByCountryCode(@Param("code") String countryCode);
 }
